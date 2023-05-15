35abfd629b6c1838ad3a35045317e5cbc58ccca8
Prince2332 <santal.prince@gmail.com>
2023-05-15 23:59:23 +0530

    Login page completed and working fine without adding static django template language syntax

--- CSV/settings.py
+++ CSV/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CSV_Handler'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,10 @@ ROOT_URLCONF = 'CSV.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / '/CSV_Handler/templates',
+            BASE_DIR / '/CSV_Handler/static',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/CSV_Handler/static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/CSV_Handler/static/'),
+                    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'resources')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
